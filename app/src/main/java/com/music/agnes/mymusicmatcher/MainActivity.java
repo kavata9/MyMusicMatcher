a42a08a33e2b190df020f83cc99a02ae8884cde6
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-06 21:34:02 +0300

    add new activity musicselector

--- app/src/main/java/com/music/agnes/mymusicmatcher/MainActivity.java
+++ app/src/main/java/com/music/agnes/mymusicmatcher/MainActivity.java
@@ -1,10 +1,10 @@
 package com.music.agnes.mymusicmatcher;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private Button mFindMusicMatcherButton;
@@ -13,12 +13,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mFindMusicMatcherButton = (Button) findViewById(R.id.findButton);
         mFindMusicMatcherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, MusicSelectorActivity.class);
+                startActivity(intent);
             }
         });
     }
