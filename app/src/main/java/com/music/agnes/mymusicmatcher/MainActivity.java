e27871cc7dbccda78cb9a19d18a6bfcb0ea07d27
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-06 22:10:33 +0300

    add function to accept user inpute for the two activities to communicate

--- app/src/main/java/com/music/agnes/mymusicmatcher/MainActivity.java
+++ app/src/main/java/com/music/agnes/mymusicmatcher/MainActivity.java
@@ -3,20 +3,28 @@ package com.music.agnes.mymusicmatcher;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindMusicMatcherButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindMusicMatcherButton = (Button) findViewById(R.id.findButton);
         mFindMusicMatcherButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, MusicSelectorActivity.class);
                 startActivity(intent);
             }
