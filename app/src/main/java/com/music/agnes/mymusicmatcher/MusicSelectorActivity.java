3af7e2c5293f81e31b4b9b42096af9a94da7d762
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-07 07:37:32 +0300

    add function to gather data from user using intent extra

--- app/src/main/java/com/music/agnes/mymusicmatcher/MusicSelectorActivity.java
+++ app/src/main/java/com/music/agnes/mymusicmatcher/MusicSelectorActivity.java
@@ -1,13 +1,21 @@
 package com.music.agnes.mymusicmatcher;
 
+import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MusicSelectorActivity extends Activity {
+    private TextView mTypeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_selector);
+        mTypeTextView = (TextView) findViewById(R.id.typeTextView);
+        Intent intent = getIntent();
+        String type = intent.getStringExtra("type");
+        mTypeTextView.setText("Here are all the music type list: " + type);
     }
 }
