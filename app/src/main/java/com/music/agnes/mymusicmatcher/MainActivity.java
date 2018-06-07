a9d5411b33fb8e0bd9bafb5dc754722efededc01
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-07 16:27:08 +0300

    change text font

--- app/src/main/java/com/music/agnes/mymusicmatcher/MainActivity.java
+++ app/src/main/java/com/music/agnes/mymusicmatcher/MainActivity.java
@@ -2,15 +2,18 @@ package com.music.agnes.mymusicmatcher;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private Button mFindMusicMatcherButton;
     private EditText mTypeEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         mTypeEditText = (EditText) findViewById(R.id.typeEditText);
         mFindMusicMatcherButton = (Button) findViewById(R.id.findButton);
+        mAppNameTextView = (TextView) findViewById(R.id.textView);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
 
         mFindMusicMatcherButton.setOnClickListener(new View.OnClickListener() {
             @Override
