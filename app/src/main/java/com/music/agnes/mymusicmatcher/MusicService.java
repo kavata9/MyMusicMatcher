f5a1b637fa45b1cebff0b5aeb6be8ad5e57d95a7
kavata9 <kavata24@gmail.com>
2018-06-16 09:38:56 +0300

    introduce class MusicService

---
+++ app/src/main/java/com/music/agnes/mymusicmatcher/MusicService.java
@@ -0,0 +1,11 @@
+package com.music.agnes.mymusicmatcher;
+
+import okhttp3.Callback;
+
+public class MusicService {
+
+    public static void findArtists(String name, Callback callback) {
+
+    }
+
+}
