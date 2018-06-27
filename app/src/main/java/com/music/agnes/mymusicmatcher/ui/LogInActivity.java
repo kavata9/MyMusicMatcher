a603e8d0e8b3e687773ba5c4c070197a8a9f8ae9
kavata9 <kavata24@gmail.com>
2018-06-27 09:59:12 +0300

    add create account page

--- app/src/main/java/com/music/agnes/mymusicmatcher/ui/LogInActivity.java
+++ app/src/main/java/com/music/agnes/mymusicmatcher/ui/LogInActivity.java
@@ -1,15 +1,36 @@
 package com.music.agnes.mymusicmatcher.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.music.agnes.mymusicmatcher.R;
 
-public class LogInActivity extends Activity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LogInActivity extends Activity  implements View.OnClickListener {
+
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LogInActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
